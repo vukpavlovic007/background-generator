f1c1dcac20adbf743464dc7be3d4b1f64ee20df4
Todor Pavlovic <vukpavlovic007@gmail.com>
2022-07-23 21:26:52 +0200

    something

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const javascript = {
 }
 
 const body = document.querySelector("body")
+
+const update = () => {
+  for (let i = 0; i < Object.keys(javascript).length; i++) {
+    console.log("updating");
+  }
+}
